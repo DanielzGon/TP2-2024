10ce82beb6a63ef58f221a69d5ddeb35d29669af
Daniel Gonçalves <143619925+DanielzGon@users.noreply.github.com>
2024-07-10 10:26:46 -0300

    Regra de três

--- servidor.js
+++ servidor.js
@@ -3,12 +3,24 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 app.get('/', (request, response) => {
+    response.render('conta')
+})
+app.get('/resultados', (request, response) => {
+    var value1 = request.query.valor1
+    var value2 = request.query.valor2
+    var value3 = request.query.valor3
 
-const dataAtual = new Date();
-const horarioAtual = dataAtual.getHours()
-const minutoAtual = dataAtual.getMinutes()
-
- response.send('A data e hora atual é: ' + dataAtual)
+    var resultadofinal = value1 * value2 / value3
+    if (resultadofinal == NaN) {
+       resultadofinal = ("Erro, não é um número")
+    }
+    else{
+        response.render('result', {r: resultadofinal})
+    }
 
 })
-app.listen(8080)
+
+const porta = 7777
+app.listen(porta, () => {
+    console.log(`Servidor funcionando na porta: ${porta}`)
+})
