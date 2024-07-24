95c677372c80d5c9303a8ce2a06702d4d9d6dd45
DanielzGon <danielgoncalvescarvalho12@gmail.com>
2024-07-24 20:22:58 -0300

    commit para envio

--- Aula17.07/servidor.js
+++ Aula17.07/servidor.js
@@ -1,23 +1,36 @@
-const express = require('express')
-const app = express()
-app.use(express.urlencoded({ extended: true }))
-app.set('view engine', 'ejs')
+
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+
 app.get('/', (request, response) => {
-    response.render('index')
-})
+    response.render('index'); 
+});
 
 app.get('/resultado', (request, response) => {
-    n1 = parseFloat(request.query.n1)
-    n2 = parseFloat(request.query.n2)
-    resultado = 0
-    if (request.query.operacao == '+') {
-        result = (n1.value) + (n2.value);
+    const n1 = parseFloat(request.query.n1);
+    const n2 = parseFloat(request.query.n2);
+    let resultado;
+
+    if (request.query.operacao === '+') {
+        resultado = n1 + n2;
+    } else if (request.query.operacao === '-') {
+        resultado = n1 - n2;
+    } else if (request.query.operacao === '*') {
+        resultado = n1 * n2;
+    } else if (request.query.operacao === '/') {
+        resultado = n1 / n2;
+    } else {
+        resultado = 'Operação inválida';
     }
-    response.render('resultado')
 
-})
+    response.render('resultado', { resultado }); 
+});
 
-const porta = 7777
+const porta = 7777;
 app.listen(porta, () => {
-    console.log(`Servidor funcionando na porta: ${porta}`)
-})
+    console.log(`Servidor funcionando na porta: ${porta}`);
+});
